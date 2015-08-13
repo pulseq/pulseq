b9d6c2e81942d53da50dae6efb111e0adc0c1167
Kelvin Layton <kelvin.layton@uniklinik-freiburg.de>
2015-08-13 10:39:01 +0200

    Compare files independent of end-of-line

--- matlab/testmatlab.py
+++ matlab/testmatlab.py
@@ -5,7 +5,18 @@ print "Testing matlab code for open MRI format"
 print "======================================="
 
 from subprocess import call
-import filecmp
+
+def cmp_lines(path_1, path_2):
+	"""Compare two files, ignoring line-endings"""
+	l1 = l2 = ' '
+	with open(path_1, 'r') as f1:
+		with open(path_2, 'r') as f2:
+			while l1 != '' and l2 != '':
+				l1 = f1.readline()
+				l2 = f2.readline()
+				if l1 != l2:
+					return False
+	return True
 
 sequences = ['fid','gre','press']
 base_dir = '../examples/'
@@ -18,11 +29,11 @@ for index, seq in enumerate(sequences):
 cellStr += "}"
 
 funCmd = "parsemr({0}); exit;".format(cellStr)
-matlabCmd = 'matlab -nodisplay -nosplash -r "{0}"'.format(funCmd)
+matlabCmd = 'matlab -wait -nodisplay -nosplash -r "{0}"'.format(funCmd)
 call(matlabCmd,shell=True)
 
 for index, seq in enumerate(sequences):
-    same = filecmp.cmp(base_dir + seq + '.matlab.out',approved_dir + seq + '.matlab.out')
+    same = cmp_lines(base_dir + seq + '.matlab.out',approved_dir + seq + '.matlab.out')
 
     result = "ok" if same else "not ok"
     print "Comparing output {0}: {1}".format(seq,result)
--- src/testparser.py
+++ src/testparser.py
@@ -6,7 +6,18 @@ print "Testing parser for open MRI format"
 print "=================================="
 
 from subprocess import call
-import filecmp
+
+def cmp_lines(path_1, path_2):
+	"""Compare two files, ignoring line-endings"""
+	l1 = l2 = ' '
+	with open(path_1, 'r') as f1:
+		with open(path_2, 'r') as f2:
+			while l1 != '' and l2 != '':
+				l1 = f1.readline()
+				l2 = f2.readline()
+				if l1 != l2:
+					return False
+	return True
 
 sequences = ['fid','gre','press']
 base_dir = '../examples/'
@@ -18,13 +29,12 @@ for index, seq in enumerate(sequences):
 	with open(base_dir + seq + '.out', 'w') as f:
 
 		call(["./parsemr",base_dir + seq + '.seq'],stdout=f)
-	
-	same = filecmp.cmp(base_dir + seq + '.out',approved_dir + seq + '.out')
+
+	same = cmp_lines(base_dir + seq + '.out',approved_dir + seq + '.out')
 
 	result = "ok" if same else "not ok"
 	print "Comparing output {0}: {1}".format(seq,result)
-	
+
 	ok = ok & same
 
 exit(0 if ok else 1)
-
