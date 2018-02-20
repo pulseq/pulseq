78fe709b5738449f3b301b18c5506ac82d87259d
Stefan Kroboth <stefan.kroboth@uniklinik-freiburg.de>
2018-02-20 13:05:09 +0000

    Bugfix in writeFid.m
    To run successfully on a scanner, rfRingdownTime and rfDeadTime need to
    be provided.

--- matlab/writeFid.m
+++ matlab/writeFid.m
@@ -1,12 +1,14 @@
-seq=mr.Sequence();              % Create a new sequence object
+system = mr.opts('rfRingdownTime', 30e-6, 'rfDeadtime', 100e-6);
+
+seq=mr.Sequence(system);              % Create a new sequence object
 Nx=256;
 Nrep=1;
 
 % Create non-selective pulse 
-rf = mr.makeBlockPulse(pi/2,'Duration',0.1e-3);
+rf = mr.makeBlockPulse(pi/2,'Duration',0.1e-3, 'system', system);
 
 % Define delays and ADC events
-adc = mr.makeAdc(Nx,'Duration',3.2e-3);
+adc = mr.makeAdc(Nx,'Duration',3.2e-3, 'system', system);
 delayTE=20e-3;
 delayTR=1000e-3;
 
