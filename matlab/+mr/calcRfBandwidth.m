5b18f9995a2669daf10644b21fdad779f408427c
m-a-x-i-m-z <max.zaitsev@gmx.net>
2021-03-19 00:02:24 +0100

    added an optional parameter 'cutoff' to mr.calcRfBandwidth()

--- matlab/+mr/calcRfBandwidth.m
+++ matlab/+mr/calcRfBandwidth.m
@@ -1,9 +1,13 @@
-function [bw,spectrum,w]=calcRfBandwidth(rf)
+function [bw,spectrum,w]=calcRfBandwidth(rf, cutoff)
 %calcRfbandwidth Calculate the spectrum of the RF pulse
 %   Returns the bandwidth of the pulse and optionally the spectrum and the
-%   frequency axis
+%   frequency axis. The default for the optional parameter 'cutoff' is 0.5 
 %
 
+if nargin==1
+    cutoff=0.5;
+end
+    
 tc=mr.calcRfCenter(rf);
 
 % resample the pulse to a resonable time array
@@ -16,23 +20,23 @@ rfs=interp1(rf.t-tc,rf.signal,tt,'linear',0);
 spectrum=fftshift(fft(fftshift(rfs)));
 w=(-floor(nn/2):ceil(nn/2)-1)*dw;
 
-w1=findFlank(w,spectrum);
-w2=findFlank(w(end:-1:1),spectrum(end:-1:1));
+w1=findFlank(w,spectrum,cutoff);
+w2=findFlank(w(end:-1:1),spectrum(end:-1:1),cutoff);
 
 bw=w2-w1;
 
 end
 
 
-function xf=findFlank(x,f)
+function xf=findFlank(x,f,c)
 
 m=max(abs(f));
-f=abs(f)/m;
-[~,i]=find(f>0.5,1);
+f=abs(f)/m-c;
+[~,i]=find(f>0,1);
 if i>1
     f0=f(i-1);
     f1=f(i);
-    xf=((f1-0.5)*x(i-1)+(0.5-f0)*x(i))/(f1-f0); 
+    xf=(f1*x(i-1)-f0*x(i))/(f1-f0); 
 else
     xf=x(1);
 end
