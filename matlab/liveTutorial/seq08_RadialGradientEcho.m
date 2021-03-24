98fc7102ebf5aa205da6833f20920d4c1ef23b58
m-a-x-i-m-z <max.zaitsev@gmx.net>
2021-03-24 20:36:14 +0100

    minor updates, change to a full circle to allow for anisotropic delay calibration

--- matlab/liveTutorial/seq08_RadialGradientEcho.m
+++ matlab/liveTutorial/seq08_RadialGradientEcho.m
@@ -4,9 +4,9 @@ alpha=30;                       % flip angle
 sliceThickness=3e-3;            % slice
 TE=8e-3;                        % TE; give a vector here to have multiple TEs (e.g. for field mapping)
 TR=20e-3;                      % only a single value for now
-Nr=128;                         % number of radial spokes
+Nr=256;                         % number of radial spokes
 Ndummy=20;                      % number of dummy scans
-delta=pi / Nr;                  % angular increment; try golden angle pi*(3-5^0.5) or 0.5 of it
+delta=2*pi / Nr;                  % angular increment; try golden angle pi*(3-5^0.5) or 0.5 of it
 
 % more in-depth parameters
 rfSpoilingInc=117;              % RF spoiling increment
@@ -72,14 +72,15 @@ seq.write('gre_rad.seq')       % Write to pulseq file
 %seq.install('siemens');
 
 return;
+
 %% trajectory calculation
-[ktraj_adc, ktraj, t_excitation, t_refocusing, t_adc] = seq.calculateKspace();
+%[ktraj_adc1, t_adc1, ktraj1, t_ktraj1, t_excitation1, t_refocusing1] = seq.calculateKspacePP();
+[ktraj_adc, t_adc, ktraj, t_ktraj, t_excitation, t_refocusing] = seq.calculateKspacePP('trajectory_delay',[0 0 0]*1e-6); % play with anisotropic trajectory delays -- zoom in to see the trouble ;-)
 
 % plot k-spaces
-time_axis=(1:(size(ktraj,2)))*sys.gradRasterTime;
-figure; plot(time_axis, ktraj'); % plot the entire k-space trajectory
-hold; plot(t_adc,ktraj_adc(1,:),'.'); % and sampling points on the kx-axis
+figure; plot(t_ktraj, ktraj'); % plot the entire k-space trajectory
+hold on; plot(t_adc,ktraj_adc(1,:),'.'); % and sampling points on the kx-axis
 figure; plot(ktraj(1,:),ktraj(2,:),'b'); % a 2D plot
 axis('equal'); % enforce aspect ratio for the correct trajectory display
-hold;plot(ktraj_adc(1,:),ktraj_adc(2,:),'r.'); % plot the sampling points
+hold on;plot(ktraj_adc(1,:),ktraj_adc(2,:),'r.'); % plot the sampling points
 
