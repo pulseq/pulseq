2e06066b3dbf62fca65ac498a1deeb4c5445134a
Maxim Zaitsev <maxim.zaitsev@uniklinik-freiburg.de>
2023-12-05 17:23:14 +0100

    fixes tothe 2D GRE with GRAPPA

--- matlab/demoSeq/writeGradientEcho_grappa.m
+++ matlab/demoSeq/writeGradientEcho_grappa.m
@@ -1,67 +1,62 @@
-%% ISMRM virtual meeting. Nov. 16, 2023. Qingping Chen
-% Implement GRAPPA acceleration to the 2D GRE sequence with optimized 
-% spoiler and accelerated computation. Use LABEL extension to produce raw
-% data reconstructable by the integrated ICE/Gadgetron reconstructio on the
-% scanner
-clear all; close all; clc ;
+% this is a demo GRE sequence, which uses LABEL extension to produce raw
+% data reconstuctable by the integrated image reconstruction on the scanner
 
-%% set system limits and parameters
-sys = mr.opts('MaxGrad', 22, 'GradUnit', 'mT/m', ...
-    'MaxSlew', 120, 'SlewUnit', 'T/m/s', ...
-    'rfRingdownTime', 20e-6, 'rfDeadTime', 100e-6, 'adcDeadTime', 20e-6) ;
+% set system limits
+sys = mr.opts('MaxGrad', 28, 'GradUnit', 'mT/m', ...
+    'MaxSlew', 150, 'SlewUnit', 'T/m/s', 'rfRingdownTime', 20e-6, ...
+    'rfDeadTime', 100e-6, 'adcDeadTime', 10e-6);
 
-seq = mr.Sequence(sys) ;           % Create a new sequence object
-fov = 256e-3 ; Nx = 256 ; Ny = 256 ;     % Define FOV and resolution
-alpha = 10 ;                       % flip angle
-sliceThickness = 3e-3 ;            % slice
+seq=mr.Sequence(sys);         % Create a new sequence object
+
+fov=256e-3; Nx=64; Ny=Nx; % Define FOV and resolution
+alpha=10;                  % flip angle
+thickness=3e-3;            % slice
+Nslices=1;
 sliceGap = 1e-3 ;
-TR = 20e-3 ;                       % repetition time TR
-TE = 6e-3 ;                        % echo time TE
-roDuration = 5.12e-3 ;              % ADC duration
+TR=30e-3; 
+TE=4.3e-3;
+%TE=[7.38 9.84 15]*1e-3;            % alternatively give a vector here to have multiple TEs (e.g. for field mapping)
 
-%% Create alpha-degree slice selection pulse and gradient
+% more in-depth parameters
+rfSpoilingInc=117;              % RF spoiling increment
+roDuration=3.2e-3;              % ADC duration
+
+% Create fat-sat pulse 
+% (in Siemens interpreter from January 2019 duration is limited to 8.192 ms, and although product EPI uses 10.24 ms, 8 ms seems to be sufficient)
+% B0=2.89; % 1.5 2.89 3.0
+% sat_ppm=-3.45;
+% sat_freq=sat_ppm*1e-6*B0*lims.gamma;
+% rf_fs = mr.makeGaussPulse(110*pi/180,'system',lims,'Duration',8e-3,...
+%     'bandwidth',abs(sat_freq),'freqOffset',sat_freq);
+% gz_fs = mr.makeTrapezoid('z',sys,'delay',mr.calcDuration(rf_fs),'Area',1/1e-4); % spoil up to 0.1mm
+
+% Create alpha-degree slice selection pulse and gradient
 [rf, gz] = mr.makeSincPulse(alpha*pi/180,'Duration',3e-3,...
-    'SliceThickness',sliceThickness,'apodization',0.42,'timeBwProduct',4,'system',sys);
+    'SliceThickness',thickness,'apodization',0.42,'timeBwProduct',4,'system',sys);
 
-%% Define other gradients and ADC events
-deltak = 1/fov ;
-gx = mr.makeTrapezoid('x','FlatArea',Nx*deltak,'FlatTime',roDuration,'system',sys) ;
-adc = mr.makeAdc(Nx,'Duration',gx.flatTime,'Delay',gx.riseTime,'system',sys) ;
-gxPre = mr.makeTrapezoid('x','Area',-gx.amplitude*(adc.dwell*(adc.numSamples/2+0.5)+0.5*gx.riseTime),...
-    'Duration',1e-3,'system',sys) ;
-gzReph = mr.makeTrapezoid('z','Area',-gz.area/2,'Duration',1e-3,'system',sys) ;
-phaseAreas = ((0:Ny-1) - Ny/2) * deltak ;
-gy = mr.makeTrapezoid('y','Area',max(abs(phaseAreas)),'Duration',mr.calcDuration(gxPre),'system',sys) ;
-peScales = phaseAreas/gy.area ;
-clear gyPre gyReph ;
-for iY = 1:Ny
-    gyPre(iY) = mr.scaleGrad(gy, peScales(iY)) ;
-    gyReph(iY) = mr.scaleGrad(gy, -peScales(iY)) ;
-end
-%% gradient spoiling
-% we cut the RO gradient into two parts for the optimal spoiler timing
-[gx1, ~] = mr.splitGradientAt(gx, gx.riseTime + gx.flatTime) ;
+% Define other gradients and ADC events
+deltak=1/fov;
+gx = mr.makeTrapezoid('x','FlatArea',Nx*deltak,'FlatTime',roDuration,'system',sys);
+adc = mr.makeAdc(Nx,'Duration',gx.flatTime,'Delay',gx.riseTime,'system',sys);
+gxPre = mr.makeTrapezoid('x','Area',-gx.area/2,'Duration',1e-3,'system',sys);
+gzReph = mr.makeTrapezoid('z','Area',-gz.area/2,'Duration',1e-3,'system',sys);
+phaseAreas = -((0:Ny-1)-Ny/2)*deltak; % phase area should be Kmax for clin=0 and -Kmax for clin=Ny... strange
+
 % gradient spoiling
-gxSpoil = mr.makeExtendedTrapezoidArea(gx.channel, gx.amplitude, 0, 2*Nx*deltak, sys) ;
-gxSpoil.delay = mr.calcDuration(gx1) ;
-gx_add = mr.addGradients({gx1, gxSpoil},'system',sys) ;
+gxSpoil=mr.makeTrapezoid('x','Area',2*Nx*deltak,'system',sys);
+gzSpoil=mr.makeTrapezoid('z','Area',4/thickness,'system',sys);
 
-gzSpoil = mr.makeTrapezoid('z','Area',4/sliceThickness,'system',sys) ;
-gzSpoil.delay = max(mr.calcDuration(gx1), gzSpoil.delay) ;
+% Calculate timing
+delayTE=ceil((TE - mr.calcDuration(gxPre) - gz.fallTime - gz.flatTime/2 ...
+    - mr.calcDuration(gx)/2)/seq.gradRasterTime)*seq.gradRasterTime;
+delayTR=ceil((TR - mr.calcDuration(gz) - mr.calcDuration(gxPre) ...
+    - mr.calcDuration(gx) - delayTE)/seq.gradRasterTime)*seq.gradRasterTime;
+assert(all(delayTE>=0));
+assert(all(delayTR>=mr.calcDuration(gxSpoil,gzSpoil)));
 
-for iY = 1:Ny
-    gyReph(iY).delay = max(mr.calcDuration(gx1), gyReph(iY).delay) ;
-end
+rf_phase=0;
+rf_inc=0;
 
-%% Calculate timing
-delayTE = ceil((TE - mr.calcDuration(gxPre) - gz.fallTime - gz.flatTime/2 ...
-    - mr.calcDuration(gx)/2)/seq.gradRasterTime) * seq.gradRasterTime ;
-delayTR = ceil((TR - mr.calcDuration(gz) - mr.calcDuration(gxPre) ...
-    - mr.calcDuration(gx_add, gzSpoil, gyReph) - delayTE)/seq.gradRasterTime)*seq.gradRasterTime ;
-assert(delayTE >= 0 ) ;
-assert(delayTR >= 0 ) ;
-
-%% implement GRAPPA pattern
 % set ACS lines for GRAPPA simulation (fully sampled central k-space
 % region)
 accelFactorPE = 2 ;
@@ -71,8 +66,15 @@ PEsamp_ACS = Ny/2-ACShw+1 : Ny/2+ACShw ; % GRAPPA autocalibration lines
 PEsamp = union(PEsamp_u, PEsamp_ACS) ; % actually sampled lines
 nPEsamp = length(PEsamp) ; % number of actually sampled
 PEsamp_INC = diff([PEsamp, PEsamp(end)]) ;
+% PEsamp indexes the actually sampled lines to the encoded k-space line number. 
+% For example, if there were just regular factor 2 undersampling 
+% (with no ACS lines), PEsamp would have length 128 and be [1 3 5 ... 255].
+% With ACS lines, the elements of PEsamp are separated by 2 near the k-space
+% edges, and by 1 in the central ACS region.
 
-%% label setting
+% all LABELS / counters an flags are automatically initialized to 0 in the beginning, no need to define initial 0's  
+% so we will just increment LIN after the ADC event (e.g. during the spoiler)
+
 % Set PAT scan flag
 % Mdh.setPATRefScan; Mdh.setPATRefAndImaScan
 lblSetRefScan = mr.makeLabel('SET','REF', true) ;
@@ -80,29 +82,33 @@ lblSetRefAndImaScan = mr.makeLabel('SET','IMA', true) ;
 lblResetRefScan = mr.makeLabel('SET','REF', false) ;
 lblResetRefAndImaScan = mr.makeLabel('SET','IMA', false) ;
 
-%% accelerate computations
-% preregister constant objects to accelerate computations
-% this is not necessary, but accelerates the sequence creation
-gxPre.id = seq.registerGradEvent(gxPre) ;
-gx1.id = seq.registerGradEvent(gx1) ;
-gzSpoil.id = seq.registerGradEvent(gzSpoil) ;
-% the phase of the RF object will change, therefore we only per-register the shapes
-% [~, rf.shapeIDs] = seq.registerRfEvent(rf) ; 
-rf.id = seq.registerRfEvent(rf) ; % NO GO EXAMPLE!!!
-for iY = 1:Ny
-    gyPre(iY).id = seq.registerGradEvent(gyPre(iY)) ;
-    gyReph(iY).id = seq.registerGradEvent(gyReph(iY)) ;
+%lblSetRefScan.id=seq.registerLabelEvent(lblSetRefScan);
+%lblSetRefAndImaScan.id=seq.registerLabelEvent(lblSetRefAndImaScan);
+%lblResetRefScan.id=seq.registerLabelEvent(lblResetRefScan);
+%lblResetRefAndImaScan.id=seq.registerLabelEvent(lblResetRefAndImaScan);
+
+% Add noise scans.
+seq.addBlock(mr.makeLabel('SET', 'LIN', 0)) ;
+seq.addBlock(adc, mr.makeLabel('SET', 'NOISE', true),lblResetRefScan,lblResetRefAndImaScan) ;
+seq.addBlock(mr.makeLabel('SET', 'NOISE', false)) ;
+
+if ismember(PEsamp(1),PEsamp_ACS)
+    if ismember(PEsamp(1),PEsamp_u)
+        seq.addBlock(mr.makeLabel('SET', 'LIN', PEsamp(1)-1), lblSetRefAndImaScan, lblSetRefScan) ;
+    else
+        seq.addBlock(mr.makeLabel('SET', 'LIN', PEsamp(1)-1), lblResetRefAndImaScan, lblSetRefScan) ;
+    end
+else
+    seq.addBlock(mr.makeLabel('SET', 'LIN', PEsamp(1)-1), lblResetRefAndImaScan, lblResetRefScan) ; % Set first PE PAT scan line
 end
 
-lblSetRefScan.id = seq.registerLabelEvent(lblSetRefScan) ;
-lblSetRefAndImaScan.id = seq.registerLabelEvent(lblSetRefAndImaScan) ;
-lblResetRefScan.id = seq.registerLabelEvent(lblResetRefScan) ;
-lblResetRefAndImaScan.id = seq.registerLabelEvent(lblResetRefAndImaScan) ;
+% slice positions
+slicePositions=(thickness+sliceGap)*((0:(Nslices-1)) - (Nslices-1)/2);
+slicePositions=slicePositions([1:2:Nslices 2:2:Nslices]); % reorder slices for an interleaved acquisition (optional)
 
-%% Loop over phase encodes and define sequence blocks
-tic ;
-for count=1:nPEsamp
-    % set GRAPPA labels
+rf.freqOffset=gz.amplitude*thickness*(1-1-(Nslices-1)/2);
+% loop over phase encodes and define sequence blocks
+for count = 1:nPEsamp
     if ismember(PEsamp(count),PEsamp_ACS)
         if ismember(PEsamp(count),PEsamp_u)
             seq.addBlock(lblSetRefAndImaScan, lblSetRefScan) ;
@@ -112,19 +118,22 @@ for count=1:nPEsamp
     else
         seq.addBlock(lblResetRefAndImaScan, lblResetRefScan) ;
     end
-    % RF spoiling (vary RF phase pseudo-randomly)
-    rand_phase = mod(117*(count^2 + count + 2), 360) * pi/180 ;
-    rf.phaseOffset = rand_phase ;
-    adc.phaseOffset = rand_phase ;
+    rf.phaseOffset=rf_phase/180*pi;
+    adc.phaseOffset=rf_phase/180*pi;
+    rf_inc=mod(rf_inc+rfSpoilingInc, 360.0);
+    rf_phase=mod(rf_phase+rf_inc, 360.0);
     %
-    seq.addBlock(rf, gz) ;
-    seq.addBlock(gxPre, gyPre(PEsamp(count)), gzReph) ;
-    seq.addBlock(mr.makeDelay(delayTE)) ;
-    seq.addBlock(gx_add, adc, gzSpoil, gyReph(PEsamp(count)) ) ;
-    seq.addBlock(mr.makeDelay(delayTR) ) ;
-    seq.addBlock(mr.makeLabel('INC','LIN', PEsamp_INC(count))) ;
+    seq.addBlock(rf,gz);
+    gyPre = mr.makeTrapezoid('y','Area',phaseAreas(PEsamp(count)),'Duration',mr.calcDuration(gxPre),'system',sys);
+    seq.addBlock(gxPre,gyPre,gzReph);
+    seq.addBlock(mr.makeDelay(delayTE));
+    seq.addBlock(gx,adc);
+    gyPre.amplitude=-gyPre.amplitude;
+
+    seq.addBlock(mr.makeDelay(delayTR),gxSpoil,gyPre,gzSpoil);
+    seq.addBlock(mr.makeLabel('INC','LIN', PEsamp_INC(count)));
 end
-toc ;
+seq.addBlock(mr.makeLabel('SET', 'LIN', 0)) ;
 
 %% check whether the timing of the sequence is correct
 [ok, error_report]=seq.checkTiming;
@@ -138,35 +147,45 @@ else
 end
 
 %% prepare sequence export
-seq.setDefinition('FOV', [fov fov sliceThickness]) ;
-seq.setDefinition('Name', 'gre_p2') ;
+seq.setDefinition('FOV', [fov fov max(slicePositions)-min(slicePositions)+thickness]);
+seq.setDefinition('Name', 'gre_gt');
 % the following definitions have effect in conjunction with LABELs 
-seq.setDefinition('SlicePositions', 0) ;
-seq.setDefinition('SliceThickness', sliceThickness) ;
-seq.setDefinition('SliceGap', sliceGap) ;
-seq.write('gre_p2.seq')       % Write to pulseq file
+seq.setDefinition('SlicePositions', slicePositions);
+seq.setDefinition('SliceThickness', thickness);
+seq.setDefinition('SliceGap', sliceGap);
+
+seq.write('gre_gt.seq')       % Write to pulseq file
+
+%seq.install('siemens');
+%return
 
 %% evaluate label settings more specifically
-adc_lbl = seq.evalLabels('evolution','adc') ;
-figure ; 
-hold on ;
-plot(adc_lbl.LIN) ;
-plot(adc_lbl.REF) ;
+%seq.plot('timeRange', [0 32]*TRout, 'TimeDisp', 'ms', 'Label', 'LIN');
+adc_lbl=seq.evalLabels('evolution','adc');
+figure; plot(adc_lbl.REF);
+hold on; plot(adc_lbl.LIN);plot(adc_lbl.IMA) ; plot(adc_lbl.NOISE);
 plot(adc_lbl.IMA) ;
-
-legend('LIN','REF', 'IMA') ;
-title('evolution of labels/counters') ;
+legend('REF','LIN', 'IMA', 'NOISE');
+title('evolution of labels/counters');
 
 %% plot sequence and k-space diagrams
-seq.plot('timeRange', [0 5]*TR) ;
+
+seq.plot('timeRange', [0 256]*TR, 'TimeDisp', 'ms', 'label', 'lin');
 
 % k-space trajectory calculation
 [ktraj_adc, t_adc, ktraj, t_ktraj, t_excitation, t_refocusing] = seq.calculateKspacePP();
 
 % plot k-spaces
-figure; plot(ktraj(1,:),ktraj(2,:),'b') ; % a 2D plot
+figure; plot(t_ktraj, ktraj'); % plot the entire k-space trajectory
+hold; plot(t_adc,ktraj_adc(1,:),'.'); % and sampling points on the kx-axis
+title('k-space components as functions of time');
+figure; plot(ktraj(1,:),ktraj(2,:),'b'); % a 2D plot
 axis('equal'); % enforce aspect ratio for the correct trajectory display
 hold;plot(ktraj_adc(1,:),ktraj_adc(2,:),'r.'); % plot the sampling points
+title('2D k-space');
 
-
+%% very optional slow step, but useful for testing during development e.g. for the real TE, TR or for staying within slewrate limits  
+return ;
+rep = seq.testReport;
+fprintf([rep{:}]);
 
