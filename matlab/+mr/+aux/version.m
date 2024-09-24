558f7bffb6524e9e6f4623db102db45b34af72e4
Maxim Zaitsev <maxim.zaitsev@uniklinik-freiburg.de>
2024-09-24 15:22:25 +0200

    finaly adding mechanisms for consistent version handling (also for loaded files)

---
+++ matlab/+mr/+aux/version.m
@@ -0,0 +1,19 @@
+function [version_major, version_minor, version_revision, version_combined]=version(type)
+%version: return the relevant version information. The type of the version
+%         information is defined by the (optional) parameter.
+%         Possible version types: 
+%    'pulseq' : (default) verson of the current Matlab package
+%    'output' : version of the file written by the seq.write() function
+
+if nargin==0 || strcmp(type,'pulseq')
+    version_major=1;
+    version_minor=4;
+    version_revision=3;
+elseif strcmp(type,'output')
+    version_major=1;
+    version_minor=4;
+    version_revision=1;
+else
+    error('Unsupported version request, type=%s',type);
+end
+version_combined=1000000*version_major+1000*version_minor+version_revision;
