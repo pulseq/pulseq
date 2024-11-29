f8e2f9238aa2ff2ee6abd6360dfa0ebd0cd9cb9c
Maxim Zaitsev <maxim.zaitsev@uniklinik-freiburg.de>
2024-11-29 17:10:38 +0100

    adding many new flags

--- matlab/+mr/getSupportedLabels.m
+++ matlab/+mr/getSupportedLabels.m
@@ -1,9 +1,9 @@
 function supported_labels = getSupportedLabels()
 % auxilary function
 
-supported_labels={'SLC','SEG','REP','AVG','SET','ECO','PHS','LIN','PAR','NAV','REV','SMS', ... # these label values are copied to the corresponding MDH field on Siemens
+supported_labels={'SLC','SEG','REP','AVG','SET','ECO','PHS','LIN','PAR','ACQ','NAV','REV','SMS', ... # these label values are copied to the corresponding MDH field on Siemens
     'REF', 'IMA', ... # For parallel imaging
-    'NOISE', ... % noise adjust scan, for iPAT acceleration
+    'NOISE', ... % noise adjust scan, for parallel imaging acceleration
         'PMC', ... # for MoCo/PMC Pulseq version to recognize blocks that can be prospectively corrected for motion 
         'NOROT','NOPOS','NOSCL', ... # instruct the interpreter to ignore the position, rotation or scaling of the FOV specified on the UI 
         'ONCE', ... # a 3-state flag that instructs the interpreter to alter the sequence when executing multiple repeats as follows: blocks with ONCE==0 are executed on every repetition; ONCE==1: only the first repetition; ONCE==2: only the last repetition
