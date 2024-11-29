797efef7360f5e04c39b1c79030b9a7b42b2a6d1
Maxim Zaitsev <maxim.zaitsev@uniklinik-freiburg.de>
2024-11-29 17:07:11 +0100

    implementing the new way of passing system parameter

--- matlab/+mr/makeExtendedTrapezoid.m
+++ matlab/+mr/makeExtendedTrapezoid.m
@@ -22,7 +22,7 @@ if isempty(parser)
         @(x) any(validatestring(x, validChannels)));
     parser.addParamValue('times', 0, @isnumeric);
     parser.addParamValue('amplitudes', 0, @isnumeric);
-    parser.addParamValue('system', mr.opts(), @isstruct);
+    parser.addParamValue('system', [], @isstruct);
     parser.addParamValue('maxGrad', 0, @isnumeric);
     parser.addParamValue('maxSlew', 0, @isnumeric);
     parser.addParamValue('skip_check', false);
@@ -31,6 +31,12 @@ if isempty(parser)
 end
 parse(parser,channel,varargin{:});
 opt = parser.Results;
+
+if isempty(opt.system)
+    system=mr.opts();
+else
+    system=opt.system;
+end
 
 if any(size(opt.times) ~= size(opt.amplitudes))
     error('Times and amplitudes must have the same length.');
@@ -44,7 +50,7 @@ if any(diff(opt.times)<=0)
     error('Times must be in ascending order and all times must be distinct.');
 end
 
-if abs(round(opt.times(end)/opt.system.gradRasterTime)*opt.system.gradRasterTime-opt.times(end))>1e-8 % 10ns is an acceptable rounding error
+if abs(round(opt.times(end)/system.gradRasterTime)*system.gradRasterTime-opt.times(end))>1e-8 % 10ns is an acceptable rounding error
     error('The last time point must be on a gradient raster.');
 end
 
@@ -56,8 +62,8 @@ if opt.skip_check == false && opt.times(1) > 0 && opt.amplitudes(1) ~= 0
     error('If first amplitude of a gradient is nonzero, it must connect to previous block!');
 end
 
-maxSlew = opt.system.maxSlew;
-maxGrad = opt.system.maxGrad;
+maxSlew = system.maxSlew;
+maxGrad = system.maxGrad;
 if opt.maxGrad > 0
     maxGrad = opt.maxGrad;
 end
@@ -67,23 +73,23 @@ end
 
 if (opt.convert2arbitrary)
     % represent the extended trapezoid on the regularly sampled time grid
-    waveform = mr.pts2waveform(opt.times, opt.amplitudes, opt.system.gradRasterTime);
-    grad = mr.makeArbitraryGrad(channel, waveform, opt.system, ...
+    waveform = mr.pts2waveform(opt.times, opt.amplitudes, system.gradRasterTime);
+    grad = mr.makeArbitraryGrad(channel, waveform, system, ...
                                 'maxSlew', maxSlew,...
                                 'maxGrad', maxGrad,...
                                 'delay', opt.times(1));
 else
     % keep the original possibly irregular sampling
-    if any(abs(round(opt.times/opt.system.gradRasterTime)*opt.system.gradRasterTime-opt.times)>1e-8) % 10ns is an acceptable rounding error
+    if any(abs(round(opt.times/system.gradRasterTime)*system.gradRasterTime-opt.times)>1e-8) % 10ns is an acceptable rounding error
         error('All time points must be on a gradient raster or "convert2arbitrary" option must be used.');
     end
     %
     grad.type = 'grad';
     grad.channel = opt.channel;
     grad.waveform = opt.amplitudes;
-    grad.delay = round(opt.times(1)/opt.system.gradRasterTime)*opt.system.gradRasterTime;
+    grad.delay = round(opt.times(1)/system.gradRasterTime)*system.gradRasterTime;
     grad.tt = opt.times - grad.delay;
-    grad.shape_dur = round(opt.times(end)/opt.system.gradRasterTime)*opt.system.gradRasterTime;
+    grad.shape_dur = round(opt.times(end)/system.gradRasterTime)*system.gradRasterTime;
     grad.area=0.5*sum((grad.tt(2:end)-grad.tt(1:end-1)).*(grad.waveform(2:end)+grad.waveform(1:end-1)));
 end
 
@@ -98,7 +104,7 @@ end
 
 % figure; plot(waveform)
 % waveform = waveform(1:end-2);
-% opt.times = round(opt.times/opt.system.gradRasterTime)*opt.system.gradRasterTime; % round onto grid
+% opt.times = round(opt.times/system.gradRasterTime)*system.gradRasterTime; % round onto grid
 % times_diff = diff(opt.times);
 % amplitudes_diff = diff(opt.amplitudes);
 % waveform = [];
@@ -108,10 +114,10 @@ end
 %     if ii == length(opt.times)-1
 %         crop = 0;
 %     else
-%         crop = opt.system.gradRasterTime;
+%         crop = system.gradRasterTime;
 %     end
 %     y = amplitudes_diff(ii)/times_diff(ii)*...
-%         (0:opt.system.gradRasterTime:(opt.times(ii+1)-opt.times(ii)-crop))...
+%         (0:system.gradRasterTime:(opt.times(ii+1)-opt.times(ii)-crop))...
 %         + opt.amplitudes(ii);
 %     waveform = [waveform y(1:end)];
 % end
