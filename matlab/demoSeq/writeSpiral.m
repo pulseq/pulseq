cb3989c623869e9e4e943e486c03625cdbfe9921
Maxim Zaitsev <maxim.zaitsev@uniklinik-freiburg.de>
2024-12-04 16:01:50 +0100

    massive clean-up in the example spiral sequence, more accurate trajectory calculation, support for oversampling of the shaped gradients

--- matlab/demoSeq/writeSpiral.m
+++ matlab/demoSeq/writeSpiral.m
@@ -3,98 +3,89 @@
 fov=256e-3; Nx=96; Ny=Nx;  % Define FOV and resolution
 sliceThickness=3e-3;             % slice thinckness
 Nslices=1;
-Oversampling=2; % by looking at the periphery of the spiral I would say it needs to be at least 2
+adcOversampling=2; % by looking at the periphery of the spiral I would say it needs to be at least 2
 phi=pi/2; % orientation of the readout e.g. for interleaving
 
 % Set system limits
-sys = mr.opts('MaxGrad',30,'GradUnit','mT/m',...
-    'MaxSlew',120,'SlewUnit','T/m/s',...
+sys = mr.opts('MaxGrad',20,'GradUnit','mT/m',...
+    'MaxSlew',200,'SlewUnit','T/m/s',...
     'rfRingdownTime', 30e-6, 'rfDeadtime', 100e-6, 'adcDeadTime', 10e-6, 'adcSamplesLimit', 8192);  
 seq=mr.Sequence(sys);          % Create a new sequence object
-warning('OFF', 'mr:restoreShape'); % restore shape is not compatible with spirals and will throw a warning from each plot() or calcKspace() call
+%warning('OFF', 'mr:restoreShape'); % restore shape is not compatible with spirals and will throw a warning from each plot() or calcKspace() call
 
 % Create fat-sat pulse 
 % (in Siemens interpreter from January 2019 duration is limited to 8.192 ms, and although product EPI uses 10.24 ms, 8 ms seems to be sufficient)
 B0=2.89; % 1.5 2.89 3.0
 sat_ppm=-3.45;
 sat_freq=sat_ppm*1e-6*B0*sys.gamma;
-rf_fs = mr.makeGaussPulse(110*pi/180,'system',sys,'Duration',8e-3,...
-    'bandwidth',abs(sat_freq),'freqOffset',sat_freq);
+rf_fs = mr.makeGaussPulse(110*pi/180,'system',sys,'Duration',8e-3,'dwell',10e-6,...
+    'bandwidth',abs(sat_freq),'freqOffset',sat_freq,'use','saturation');
 gz_fs = mr.makeTrapezoid('z',sys,'delay',mr.calcDuration(rf_fs),'Area',1/1e-4); % spoil up to 0.1mm
 
 % Create 90 degree slice selection pulse and gradient
-[rf, gz] = mr.makeSincPulse(pi/2,'system',sys,'Duration',3e-3,...
-    'SliceThickness',sliceThickness,'apodization',0.5,'timeBwProduct',4);
+[rf, gz, gzReph] = mr.makeSincPulse(pi/2,'system',sys,'Duration',3e-3,...
+    'SliceThickness',sliceThickness,'apodization',0.5,'timeBwProduct',4,'use','excitation');
 
 % define k-space parameters
 deltak=1/fov;
 kRadius = round(Nx/2);
-kSamples=round(2*pi*kRadius)*Oversampling;
-readoutTime = 4.2e-4;
+kSamples=round(2*pi*kRadius)*adcOversampling;
+tos_calculation=10; % time oversampling during the trajectory optimization, can be anything, with higher factors giving smoother trajectory but causing slower calculation
+gradOversampling=true; % oversampling of the gradient shape, can be either true or false
 
 % calculate a raw Archimedian spiral trajectory
 clear ka;
 ka(kRadius*kSamples+1)=1i; % init as complex
-for c=0:kRadius*kSamples
-    r=deltak*c/kSamples;
-    a=mod(c,kSamples)*2*pi/kSamples;
+for c=0:kRadius*kSamples*tos_calculation
+    r=deltak*c/kSamples/tos_calculation;
+    a=mod(c,kSamples*tos_calculation)*2*pi/kSamples/tos_calculation;
     ka(c+1)=r*exp(1i*a);
 end
 ka=[real(ka); imag(ka)];
+
 % calculate gradients and slew rates
-[ga, sa]=mr.traj2grad(ka);
+dt=sys.gradRasterTime/tos_calculation;
+[ga, sa]=mr.traj2grad(ka,'RasterTime',dt,'firstGradStepHalfRaster',tos_calculation==1,'conservativeSlewEstimate',true);
 
 % limit analysis
-safety_magrin=0.94; % we need that  otherwise we just about violate the slew rate due to the rounding errors
-dt_gcomp=abs(ga)/(sys.maxGrad*safety_magrin)*sys.gradRasterTime;
-dt_gabs=abs(ga(1,:)+1i*ga(2,:))/(sys.maxGrad*safety_magrin)*sys.gradRasterTime;
-dt_scomp=sqrt(abs(sa)/(sys.maxSlew*safety_magrin))*sys.gradRasterTime;
-dt_sabs=sqrt(abs(sa(1,:)+1i*sa(2,:))/(sys.maxSlew*safety_magrin))*sys.gradRasterTime;
+safety_magrin=0.99; % we need that  otherwise we just about violate the slew rate due to the rounding errors
+dt_gabs=abs(ga(1,:)+1i*ga(2,:))/(sys.maxGrad*safety_magrin)*dt;
+dt_sabs=sqrt(abs(sa(1,:)+1i*sa(2,:))/(sys.maxSlew*safety_magrin))*dt;
 
-figure;plot([dt_gabs; max(dt_gcomp); dt_sabs; max(dt_scomp)]');title('time stepping defined by gradient and slew-rate');
-
-dt_smooth=max([dt_gabs;dt_sabs]);
-dt_rough=max([dt_gcomp;dt_scomp]);
+%figure;plot([dt_gabs; max(dt_gcomp); dt_sabs; max(dt_scomp)]');title('time stepping defined by gradient and slew-rate');
+%%
+dt_opt=max([dt_gabs;dt_sabs]);
 
 % apply the lower limit not to lose the trajectory detail
-dt_min=4*sys.gradRasterTime/kSamples; % we want at least 4 points per revolution
-dt_smooth0=dt_smooth;
-dt_rough0=dt_rough;
-dt_smooth(dt_smooth<dt_min)=dt_min;
-dt_rough(dt_rough<dt_min)=dt_min;
+dt_min=4*sys.gradRasterTime/kSamples/tos_calculation; % we want at least 4 points per revolution
+dt_opt0=dt_opt;
+dt_opt(dt_opt<dt_min)=dt_min;
 
-figure;plot([dt_smooth0; dt_smooth; dt_rough0; dt_rough]');title('combined time stepping');
+figure;plot([dt_opt0; dt_opt]');title('combined time stepping');
 
-t_smooth=[0 cumsum(dt_smooth,2)];
-t_rough=[0 cumsum(dt_rough,2)];
+t_smooth=[0 cumsum(dt_opt,2)];
 
-kopt_smooth=interp1(t_smooth, ka', (0:floor(t_smooth(end)/sys.gradRasterTime))*sys.gradRasterTime)';
-kopt_rough=interp1(t_rough, ka', (0:floor(t_rough(end)/sys.gradRasterTime))*sys.gradRasterTime)';
+dt_grad=sys.gradRasterTime/(1+gradOversampling);
+kopt=interp1(t_smooth, ka', (0:floor(t_smooth(end)/dt_grad))*dt_grad)';
 
 % analyze what we've got
-fprintf('duration orig %d us\n', round(1e6*sys.gradRasterTime*length(ka)));
-fprintf('duration smooth %d us\n', round(1e6*sys.gradRasterTime*length(kopt_smooth)));
-fprintf('duration rough %d us\n', round(1e6*sys.gradRasterTime*length(kopt_rough)));
+fprintf('duration orig %d us\n', round(1e6*dt*length(ka)));
+fprintf('duration smooth %d us\n', round(1e6*dt_grad*length(kopt)));
 
-[gos, sos]=mr.traj2grad(kopt_smooth);
-[gor, sor]=mr.traj2grad(kopt_rough);
+[gos, sos]=mr.traj2grad(kopt,'RasterTime',dt_grad,'firstGradStepHalfRaster',gradOversampling);
 
-figure;plot([gos;abs(gos(1,:)+1i*gos(2,:))]');title('gradient with smooth (abs) constraint')
-figure;plot([gor;abs(gor(1,:)+1i*gor(2,:))]');title('gradient with rough (component) constraint')
+figure;plot([gos;abs(gos(1,:)+1i*gos(2,:))]');
+hold on; yline(sys.maxGrad,'--'); title('gradient with the abs constraint');
 
-figure;plot([sos;abs(sos(1,:)+1i*sos(2,:))]');title('slew rate with smooth (abs) constraint')
-figure;plot([sor;abs(sor(1,:)+1i*sor(2,:))]');title('slew rate with rough (component) constraint')
+figure;plot([sos;abs(sos(1,:)+1i*sos(2,:))]');
+hold on; yline(sys.maxSlew,'--'); title('slew rate with the abs constraint')
 
 % Define gradients and ADC events
 spiral_grad_shape=gos;
-% Create 90 degree slice selection pulse and gradient
-[rf, gz] = mr.makeSincPulse(pi/2,'system',sys,'Duration',3e-3,...
-    'SliceThickness',sliceThickness,'apodization',0.5,'timeBwProduct',4);
-gzReph = mr.makeTrapezoid('z',sys,'Area',-gz.area/2);
 
 % calculate ADC
 % round-down dwell time to 10 ns
-adcTime = sys.gradRasterTime*size(spiral_grad_shape,2);
+adcTime = dt_grad*size(spiral_grad_shape,2);
 % actually it is trickier than that: the (Siemens) interpreter sequence 
 % per default will try to split the trajectory into segments with the number of samples <8192
 % and every of these segments will have to have duration aligned to the
@@ -118,20 +109,29 @@ adcSamplesDesired=ceil(adcTime/adcDwell);
 [adcSegments,adcSamplesPerSegment]=mr.calcAdcSeg(adcSamplesDesired,adcDwell,sys); 
 
 adcSamples=adcSegments*adcSamplesPerSegment;
-adc = mr.makeAdc(adcSamples,'Dwell',adcDwell,'Delay',mr.calcDuration(gzReph));%lims.adcDeadTime);
+% we would like to sample the point k=1 with the furst ADC sample (i.e. at 
+% t=adcDwell/2), so we advance the ADC and round the delay to the RF raster time
+adc = mr.makeAdc(adcSamples,'Dwell',adcDwell,'Delay',round((mr.calcDuration(gzReph)-adcDwell/2)/sys.rfRasterTime)*sys.rfRasterTime);%lims.adcDeadTime);
 
 % extend spiral_grad_shape by repeating the last sample
 % this is needed to accomodate for the ADC tuning delay
-spiral_grad_shape = [spiral_grad_shape spiral_grad_shape(:,end)];
+if ~gradOversampling
+    spiral_grad_shape = [spiral_grad_shape spiral_grad_shape(:,end)];
+else
+    spiral_grad_shape = [spiral_grad_shape spiral_grad_shape(:,end) spiral_grad_shape(:,end)];
+    if mod(length(spiral_grad_shape),2)==0
+        spiral_grad_shape = [spiral_grad_shape spiral_grad_shape(:,end)]; % the vector length with oversampling must be odd
+    end
+end
 
 % readout grad 
-gx = mr.makeArbitraryGrad('x',spiral_grad_shape(1,:),'Delay',mr.calcDuration(gzReph));
-gy = mr.makeArbitraryGrad('y',spiral_grad_shape(2,:),'Delay',mr.calcDuration(gzReph));
+gx = mr.makeArbitraryGrad('x',spiral_grad_shape(1,:),'Delay',mr.calcDuration(gzReph),'first',0,'last', spiral_grad_shape(1,end),'system',sys,'oversampling',gradOversampling);
+gy = mr.makeArbitraryGrad('y',spiral_grad_shape(2,:),'Delay',mr.calcDuration(gzReph),'first',0,'last', spiral_grad_shape(2,end),'system',sys,'oversampling',gradOversampling);
 
 % spoilers
 gz_spoil=mr.makeTrapezoid('z',sys,'Area',deltak*Nx*4);
-gx_spoil=mr.makeExtendedTrapezoid('x','times',[0 mr.calcDuration(gz_spoil)],'amplitudes',[spiral_grad_shape(1,end),0]); %todo: make a really good spoiler
-gy_spoil=mr.makeExtendedTrapezoid('y','times',[0 mr.calcDuration(gz_spoil)],'amplitudes',[spiral_grad_shape(2,end),0]); %todo: make a really good spoiler
+gx_spoil=mr.makeExtendedTrapezoid('x','times',[0 mr.calcDuration(gz_spoil)],'amplitudes',[spiral_grad_shape(1,end),0],'system',sys); %todo: make a really good spoiler
+gy_spoil=mr.makeExtendedTrapezoid('y','times',[0 mr.calcDuration(gz_spoil)],'amplitudes',[spiral_grad_shape(2,end),0],'system',sys); %todo: make a really good spoiler
 
 % because of the ADC alignment requirements the sampling window possibly
 % extends past the end of the trajectory (these points will have to be
