ed0ce72a580059f7fd3613c59cc61bbfdc3004cf
Maxim Zaitsev <maxim.zaitsev@uniklinik-freiburg.de>
2025-02-11 13:35:32 +0100

    demoRead updates an little optimizations, nothing essential

--- matlab/demoUnsorted/demoRead.m
+++ matlab/demoUnsorted/demoRead.m
@@ -11,16 +11,59 @@
 % method.
 %seq_name='gre.seq'; 
 %seq_name='epi_rs.seq';
-%seq_name='spiral.seq';
-seq_name='idea/external.seq';
+seq_name='spiral.seq';
+%seq_name='idea/external.seq';
 %seq_name='trufi.seq'; 
 %seq_name='epi_se.seq';
 %seq_name='../tests/gre.seq';
 %seq_name='../tests/epi_rs.seq';
+%seq_name='/home/zaitsev/pulseq_home/open_sequence/ufr-collab/tmp/gre.seq';
+%seq_name='historical_format_tests/gre_example_120.seq';
 
 sys = mr.opts('B0', 2.89); % we need system here if we want 'detectRFuse' to detect fat-sat pulses
-seq=mr.Sequence(sys);
-seq.read(seq_name,'detectRFuse');
+seq2=mr.Sequence(sys);
+%seq.read(seq_name,'detectRFuse');
+seq2.read(seq_name);
+
+%%
+[ok, error_report]=seq2.checkTiming ;
+
+if (ok)
+    fprintf('Timing check passed successfully\n') ;
+else
+    fprintf('Timing check failed! Error listing follows:\n') ;
+    fprintf([error_report{:}]);
+    fprintf('\n');
+end
+
+seq2.plot('timeDisp','us','showBlocks',1,'timeRange',[0 25e-3]); %detailed view
+[ktraj_adc, t_adc, ktraj, t_ktraj, t_excitation, t_refocusing, slicepos, t_slicepos] = seq2.calculateKspacePP();
+figure; plot(ktraj(1,:),ktraj(2,:),'b'); % a 2D plot
+hold on;plot(ktraj_adc(1,:),ktraj_adc(2,:),'r.'); % plot the sampling points
+axis('equal'); % enforce aspect ratio for the correct trajectory display
+title('k-space trajectory (k_x/k_y)');
+
+return
+%%
+rf_ph=[];
+rf_freq=[];
+adc_ph=[];
+adc_freq=[];
+for iB=1:length(seq2.blockDurations)
+    b=seq2.getBlock(iB);
+    if ~isempty(b.rf)
+        rf_ph(end+1)=b.rf.phaseOffset;
+        rf_freq(end+1)=b.rf.freqOffset;
+    end
+    if ~isempty(b.adc)
+        adc_ph(end+1)=b.adc.phaseOffset;
+        adc_freq(end+1)=b.adc.freqOffset;
+    end
+end
+
+%%
+figure; plot(adc_ph);
+figure; plot(rf_ph);
 
 %% sanity check to see if the reading and writing are consistent
 % seq.write('read_test.seq');
@@ -30,18 +73,18 @@ seq.read(seq_name,'detectRFuse');
 % Parameters defined with in the |[DEFINITIONS]| section of the sequence file
 % are accessed with the |getDefinition| method. These are user-specified
 % definitions and do not effect the execution of the sequence.
-seqName=seq.getDefinition('Name')
+seqName=seq2.getDefinition('Name')
 
 %% calculate and display real TE, TR as well as slew rates and gradient amplitudes
 
-rep = seq.testReport; 
+rep = seq2.testReport; 
 fprintf([rep{:}]); 
 
 %%
 % Sequence blocks are accessed with the |getBlock| method. As shown in the
 % output the first block is a selective excitation block and contains an RF
 % pulse and gradient and on the z-channel.
-b1=seq.getBlock(1)
+b1=seq2.getBlock(1)
 
 %%
 % Further information about each event can be obtained by accessing the
@@ -61,9 +104,9 @@ ylabel('RF phase'); xlabel('t (ms)');
 % The next three blocks contain: three gradient events; a delay; and
 % readout gradient with ADC event, each with corresponding fields defining
 % the details of the events.
-b2 = seq.getBlock(2);
-b3 = seq.getBlock(3);
-b4 = seq.getBlock(4);
+b2 = seq2.getBlock(2);
+b3 = seq2.getBlock(3);
+b4 = seq2.getBlock(4);
 b2.gx
 %b3.delay
 b4.adc
@@ -74,7 +117,7 @@ b4.adc
 % a new figure and shows ADC, RF and gradient events. The axes are linked
 % so zooming is consistent. In this example, a simple gradient echo sequence
 % for MRI is displayed.
-seq.plot()
+seq2.plot()
 return
 %%
 % The details of individual pulses are not well-represented when the entire
@@ -82,7 +125,7 @@ return
 % Alternatively, a time range can be specified.  An additional parameter
 % also allows the display units to be changed for easy reading. 
 % Further, the handle of the created figure can be returned if required.
-fig=seq.plot('TimeRange',[0 16e-3],'timeDisp','ms')
+fig=seq2.plot('TimeRange',[0 16e-3],'timeDisp','ms')
 
 %% Modifying sequence blocks
 % In addition to loading a sequence and accessing sequence blocks, blocks
@@ -99,13 +142,13 @@ window = (1.0-alpha+alpha*cos(2*pi*t/duration));    % Hamming window
 signal = window.*sinc(BW*t);
 
 % Normalise area to achieve 2*pi rotation
-signal=signal./(seq.rfRasterTime*sum(real(signal)));
+signal=signal./(seq2.rfRasterTime*sum(real(signal)));
 
 % Scale to 45 degree flip angle
 rf2.signal=signal.*45/360;
 
 b1.rf=rf2;
-seq.setBlock(1,b1);
+seq2.setBlock(1,b1);
 
 %% second check to see what we have changed
 % seq.write('read_test2.seq');
@@ -116,5 +159,5 @@ seq.setBlock(1,b1);
 % The amplitude of the first rf pulse is reduced due to the reduced
 % flip-angle. Notice the reduction is not exactly a factor of two due to
 % the windowing function.
-amp1_in_Hz = max(abs(seq.getBlock(1).rf.signal))
-amp2_in_Hz = max(abs(seq.getBlock(6).rf.signal))
+amp1_in_Hz = max(abs(seq2.getBlock(1).rf.signal))
+amp2_in_Hz = max(abs(seq2.getBlock(6).rf.signal))
