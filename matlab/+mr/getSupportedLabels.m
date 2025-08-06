297a2f4edfba126225cffacc53b0ab293ededb24
Maxim Zaitsev <maxim.zaitsev@uniklinik-freiburg.de>
2025-08-06 17:31:38 +0200

    reformatting the getSupportedLabels.m file to include a litte bit of documentation; adding *OFF* flag to switch between MDH_ONLINE and MDH_OFFLINE on siemens platforms

--- matlab/+mr/getSupportedLabels.m
+++ matlab/+mr/getSupportedLabels.m
@@ -1,13 +1,19 @@
 function supported_labels = getSupportedLabels()
 % auxilary function
 
-supported_labels={'SLC','SEG','REP','AVG','SET','ECO','PHS','LIN','PAR','ACQ','NAV','REV','SMS', ... # these label values are copied to the corresponding MDH field on Siemens
-    'REF', 'IMA', ... # For parallel imaging
-    'NOISE', ... % noise adjust scan, for parallel imaging acceleration
+supported_labels={ ...
+    ... % data counters
+    'SLC','SEG','REP','AVG','SET','ECO','PHS','LIN','PAR','ACQ', ... % these are copied to the corresponding MDH fields on Siemens
+    ... % data flags
+    'NAV','REV','SMS', ... % these are copied to the corresponding MDH fields on Siemens 
+    'REF', 'IMA', ... % flags for parallel imaging
+    'OFF', ... % Offline flag that labels the data, that should not be used for the online-reconstruction (on Siemens it negates the ONLINE MDH flag)
+    'NOISE', ... % flag marking noise adjust scan, for parallel imaging acceleration
+    ... % control flags/switches -- they are not affecting the data but rather the sequence itself
         'PMC', ... # for MoCo/PMC Pulseq version to recognize blocks that can be prospectively corrected for motion 
         'NOROT','NOPOS','NOSCL', ... # instruct the interpreter to ignore the position, rotation or scaling of the FOV specified on the UI 
         'ONCE', ... # a 3-state flag that instructs the interpreter to alter the sequence when executing multiple repeats as follows: blocks with ONCE==0 are executed on every repetition; ONCE==1: only the first repetition; ONCE==2: only the last repetition
-        'TRID' ... # an integer ID of the TR (sequence segment) used by the GE interpreter to optimize the execution on the scanner
+        'TRID' ... # an integer ID of the TR (sequence segment) used by the GE interpreter (and some others) to optimize the execution on the scanner
     };
 
 end
