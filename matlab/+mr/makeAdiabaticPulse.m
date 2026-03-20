1ab994c72de127aabca3225fdf2f3e44ab0f96ae
Maxim Zaitsev <maxim.zaitsev@uniklinik-freiburg.de>
2026-03-20 10:04:31 +0100

    Octave compatibility fixes

--- matlab/+mr/makeAdiabaticPulse.m
+++ matlab/+mr/makeAdiabaticPulse.m
@@ -122,11 +122,11 @@ else
     % this probably only works on linux and maybe also on mac
     [status, result]=system('which python3');
     if status==0
-        python=strip(result);
+        python=mr.aux.strstrip(result);
     else
         [status, result]=system('which python');
         if status==0
-            python=strip(result);
+            python=mr.aux.strstrip(result);
         else
             error('python executable not found');
         end
