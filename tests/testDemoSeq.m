127af9c696d5043813fee5050d84b060f0dd7ec0
Maxim Zaitsev <maxim.zaitsev@uniklinik-freiburg.de>
2026-03-25 18:16:42 +0100

    adding a test including all demo sequences (they should run somehow without failures) with a possibility to exclude demo sequences that don't run (e.g. due to external dependencies) or are too slow

---
+++ tests/testDemoSeq.m
@@ -0,0 +1,52 @@
+% simple test that runs all demo sequences
+function tests = testDemoSeq
+    tests = functiontests(localfunctions);
+end
+
+function test_run_all_demo_seqs(testCase)
+    % excluded sequences (too slow or known not to run)
+    excludedSeqs={'writeMPRAGE_4ge', 'just_some_nonexistant_sequence_to_test_multiple_excludes'};
+    % prepare environment and enumerate files
+    currDir  = fileparts(which('testDemoSeq'));
+    % test whether the main toolbox is in the path
+    try
+        sys=mr.opts();
+    catch
+        addpath(fullfile(currDir,'../matlab'));
+    end
+    demoDir=fullfile(currDir,'../matlab/demoSeq');
+    d = dir(fullfile(demoDir, '*.m'));
+    demoSeqs={};
+    for k = 1:numel(d)
+        [~, name, ~] = fileparts(d(k).name);
+        safeName = matlab.lang.makeValidName(name);
+        if any(strcmp(safeName,excludedSeqs))
+            continue;
+        end
+        demoSeqs{end+1}=safeName;
+    end
+    addpath(fullfile(currDir,'../matlab/demoUnsorted')); % for fidBeep
+    addpath(demoDir);
+    % now run all sequences one after another
+    succeededSeqs={};
+    failedSeqs={};
+    for currSeq=demoSeqs
+        try
+            eval(currSeq{1});
+            succeededSeqs{end+1}=currSeq;
+            close all;
+        catch ME
+            %testCase.verifyFail(...
+            %    sprintf('Script %s threw: %s', s, ME.message));
+            fprintf('failed sequence: %s\n', currSeq{1});
+            fprintf('error message: %s\n', ME.message);
+            failedSeqs{end+1}=['failed sequence: ''' currSeq{1} ''', error message: ' ME.message ' '];
+            close all;
+        end
+    end
+    if isempty(failedSeqs)
+        testCase.verifySucceded(['Succeded sequences: ' succeededSeqs{:}]);
+    else
+        testCase.verifyFail(['Failed sequences: ' failedSeqs{:}]);
+    end
+end
