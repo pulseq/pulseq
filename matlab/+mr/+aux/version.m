f66f81a4da8a4f824cf36d00fa030277899dbdc2
Maxim Zaitsev <maxim.zaitsev@uniklinik-freiburg.de>
2026-03-25 12:20:30 +0100

    updating the package version, but not the output version

--- matlab/+mr/+aux/version.m
+++ matlab/+mr/+aux/version.m
@@ -8,7 +8,7 @@ function [version_major, version_minor, version_revision, version_combined]=vers
 if nargin==0 || strcmp(type,'pulseq')
     version_major=1;
     version_minor=5;
-    version_revision=1;
+    version_revision=2;
 elseif strcmp(type,'output')
     version_major=1;
     version_minor=5;
