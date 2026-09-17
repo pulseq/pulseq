aeefaae416590908e487f396392ec569bd7843c7
Maxim Zaitsev <maxim.zaitsev@uniklinik-freiburg.de>
2026-09-17 12:16:40 +0200

    added dummy scans

--- matlab/demoSeq/writeFid.m
+++ matlab/demoSeq/writeFid.m
@@ -4,21 +4,29 @@ system = mr.opts('rfRingdownTime', 20e-6, 'rfDeadTime', 100e-6, ...
 seq=mr.Sequence(system);              % Create a new sequence object
 Nx=4096;
 Nrep=16;
+Ndummy=4
+
+RfDuration=0.3e-3;
+AdcDuration=256e-3;
 
 % Create non-selective pulse 
-rf = mr.makeBlockPulse(pi/2,'Duration',0.3e-3, 'system', system,'use','excitation');
+rf = mr.makeBlockPulse(pi/2,'Duration', RfDuration, 'system', system,'use','excitation');
 
 % Define delays and ADC events
-adc = mr.makeAdc(Nx,'Duration',512e-3, 'system', system,'delay',system.adcDeadTime);
+adc = mr.makeAdc(Nx,'Duration', AdcDuration, 'system', system,'delay',system.adcDeadTime);
 delayTE=20e-3;
-delayTR=5000e-3;
+delayTR=10000e-3;
 %
 assert(delayTE>=mr.calcDuration(rf));
 assert(delayTR>=mr.calcDuration(adc));
 % Loop over repetitions and define sequence blocks
-for i=1:Nrep
-    seq.addBlock(rf,mr.makeDelay(delayTE));
-    seq.addBlock(adc,mr.makeDelay(delayTR));
+for i=(1-Ndummy):Nrep
+    seq.addBlock(rf,delayTE);
+    if (i>0)
+        seq.addBlock(adc,delayTR);
+    else
+        seq.addBlock(delayTR);
+    end
 end
 
 % check whether the timing of the sequence is correct
